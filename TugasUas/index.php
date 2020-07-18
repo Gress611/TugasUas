235e3f21fc67e8743f6779c5e98d636b458b97a0
Gress611 <64961291+Gress611@users.noreply.github.com>
2020-07-17 19:27:23 -0700

    Add files via upload

---
+++ TugasUas/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-scale-1.0">
+	<title>Dashboard Gracshella Tiana</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<nav>
+		<div class="logo">
+			<img src="nsk.jpg">
+		</div>
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Menu</a></li>
+			<li><a href="uas/index.php">Admin</a></li>
+			<li><a href="#">Setting</a></li>
+		</ul>
+	</nav>
+	<div class="row">
+  <div class="column">
+    <div class="card">
+      <h3>Absensi Karyawan</h3>
+      <p>-</p>
+      <p>5 Karyawan</p>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <h3>Tanggal</h3>
+      <p>-</p>
+      <p>Sunday, 10 May 2020</p>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <h3>Jadwal Karyawan</h3>
+    </div>
+  </div>
+</div>
+  <table id="customers">
+  <tr>
+    <th>Nama Karyawan</th>
+    <th>Jenis Kelamin</th>
+    <th>Tempat, Tanggal Lahir</th>
+  </tr>
+  <tr>
+    <td>Gracshella Tiana</td>
+    <td>Perempuan</td>
+    <td>Bekasi, 06 November 1998</td>
+  </tr>
+  <tr>
+    <td>Zulfa Anita</td>
+    <td>Perempuan</td>
+    <td>Bekasi, 05 Desember 1998</td>
+  </tr>
+  <tr>
+    <td>Diana Rahmawati</td>
+    <td>Perempuan</td>
+    <td>Bekasi, 11 Agustus 1999</td>
+  </tr>
+  <tr>
+    <td>Fira Wahyu Permata Sari</td>
+    <td>Perempuan</td>
+    <td>Bogor, 24 Agustus 1999</td>
+  </tr>
+  <tr>
+    <td>Fina Lestari</td>
+    <td>Perempuan</td>
+    <td>Kebumen, 13 Juni 1998</td>
+  </tr>
+  <tr>
+</table>
+
+</body>
+</html>
